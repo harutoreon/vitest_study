7887e088d9bac285b902ce18438a6a280a819499
reonharuto <norabder567@gmail.com>
2026-04-23 20:35:52 +0900

    Add setTimeout.js

---
+++ src/composables/setTimeout.js
@@ -0,0 +1,25 @@
+// setTimeout(() => {
+//   console.log('1秒後に表示されます');
+// }, 1000);
+
+// function gree(name) {
+//   console.log(`こんにちは、${name}さん！`);
+// }
+
+// setTimeout(gree, 2000, '田中');
+
+// setTimeout(() => { console.log('こんにちは') }, 1000);
+
+// const timerId = setTimeout(() => {
+//   console.log('これは表示されない');
+// }, 3000);
+
+// clearTimeout(timerId);
+
+// console.log('1番目');
+
+// setTimeout(() => {
+//   console.log('3番目');
+// }, 0);
+
+// console.log('2番目');
